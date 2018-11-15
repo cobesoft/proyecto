1833e9c190f3ed1bbbd6c38c4adec98864e8b506
uncristianr <44310532+uncristianr@users.noreply.github.com>
2018-11-15 01:05:13 -0500

    proyecto original

---
+++ index.php
@@ -0,0 +1,33 @@
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <link href="//cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet"/>
+    <link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
+    <link href="./css/styles.css" rel="stylesheet"/>
+    <title>Bancoop</title>
+  </head>
+  <body>
+    <div class="container">
+      <header class="header">
+        <h1 class="header__logo"><img src="./img/banner.jpg" alt=""/></h1>
+        <h2 class="header__slogan"><i>El Banco de Nosotros</i></h2>
+        <nav class="header__nav">
+          <ul class="header__menu">
+            <li class="header__navItem"><a href="index.html">Inicio</a></li>
+            <li class="header__navItem"><a href="#">Creditos</a></li>
+            <li class="header__navItem"><a href="#">entrar</a></li>
+            <li class="header__navItem"><a href="#">Ahorros</a></li>
+            <li class="header__navItem"><a href="#">Servicio al Cliente</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="contenido">
+        <?php include "inicio.php"; ?>
+
+      </div>
+      <footer class="footer full-width">
+        <h3 class="footer__titulo">CobeSoft - 2018. Derechos reservados</h3><span class="footer__info">Linea Gratuita 01 8000 000104</span>
+      </footer>
+    </div>
+  </body>
---
+++ inicio.php
@@ -0,0 +1,82 @@
+<section class="sidebar">
+<form class="login" action="" method="">
+  <h3 class="titulo">Iniciar sesión</h3>
+  <fieldset class="login__fieldset">
+    <label for="cuenta">Cuenta</label>
+    <input class="login__input" mane="cuenta" placeholder="Numero de tu Cuenta" type="text" tabindex="1" required="required" autofocus="autofocus"/>
+  </fieldset>
+  <fieldset class="login__fieldset">
+    <label for="tipo">Tipo de Cuenta</label>
+    <select class="login__input" name="tipo" placeholder="Tipo de Cuenta">
+      <option value="ahorros">Cuenta de Ahorros</option>
+      <option value="corriente">Cuenta Corriente</option>
+    </select>
+  </fieldset>
+  <fieldset class="login__fieldset">
+    <label for="clave">Clave Asociada</label>
+    <input class="login__input" mane="clave" placeholder="Contraseña" type="password" tabindex="3" required="required"/>
+  </fieldset>
+  <fieldset class="login__fieldset">
+    <button class="login__button" name="enviar" type="submit" data-submit="...Enviando">Enviar</button>
+  </fieldset>
+</form>
+<section class="transacciones">
+  <h3 class="titulo">Espacio de Transacciones</h3>
+  <ul>
+    <li class="transacciones__item"><a href="">Banca Seguros</a></li>
+    <li class="transacciones__item"><a href="">Banca Empresarial</a></li>
+    <li class="transacciones__item"><a href="">Banca Personal</a></li>
+    <li class="transacciones__item"><a href="">Pago de las Facturas</a></li>
+  </ul>
+</section>
+<section class="tarjetas">
+  <h3 class="titulo">Tarjetas de Credito</h3>
+  <ul>
+    <li class="tarjetas__item"><a href="">Credi Visa</a></li>
+    <li class="tarjetas__item"><a href="">Credi MasterCard</a></li>
+  </ul>
+</section>
+</section>
+<section class="content">
+<div class="row">
+  <div class="contenido">
+    <article class="entrada">
+      <h2 class="entrada__titulo">Productos para ti</h2><img class="entrada__img" src="./img/tarjetascredito.jpg" alt=""/>
+      <p class="entrada__parrafo">
+        acercate para que conoscas los productos de nuestro banco que tiene disponibles para ti.
+        Solicitalo de una forma facil y rapida. Solo debes escoger el prooducto
+        e ingresar tus datos de Contacto y hacemos el tramite y listo . En poco tiempo un asesor se comunicara contigo para que todo este correcto.
+      </p><a class="entrada__link" href="#">Leer Mas</a>
+    </article>
+    <article class="entrada">
+      <h2 class="entrada__titulo">Ahorro Estudiantil</h2><img class="entrada__img" src="./img/esttudiante.jpg" alt=""/>
+      <p class="entrada__parrafo">
+        Desde ya puedes empezar a ahorrar para la educación de
+        tus hijos, complementándola con un seguro estudiantil para tu bienestar y el de tu familia asegurandoles un futuro
+        y con sorteos de mutiples premios el cual puedes ganar
+      </p><a class="entrada__link" href="#">Leer Mas</a>
+    </article>
+  </div>
+</div>
+<div class="row">
+  <div class="contenido">
+    <article class="entrada">
+      <h2 class="entrada__titulo">Credito Para Vehiculos</h2><img class="entrada__img" src="./img/vehiculo.jpg" alt=""/>
+      <p class="entrada__parrafo">
+        Solicita tu crédito  para financiar la compra
+        de Vehiculos nuevos o usados, particulares o comerciales
+        directamente o a traves de un concesionario asi que dale.
+      </p><a class="entrada__link" href="#">Leer Mas</a>
+    </article>
+    <article class="entrada">
+      <h2 class="entrada__titulo">Credito Hipotecario</h2><img class="entrada__img" src="./img/hipotecario.jpg" alt=""/>
+      <p class="entrada__parrafo">
+        Un inmueble propio se ha convertido en el más anhelado
+        sueño. Por eso es nesesario eligir el inmueble que esté acorde
+        a las espectativas al igual que el crédito que mejor se adapte
+        a tus posibilidades
+      </p><a class="entrada__link" href="#">Leer Mas</a>
+    </article>
+  </div>
+</div>
+</section>
